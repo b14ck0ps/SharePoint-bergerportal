091de99f22222038f2116b408dd033140681cef9
Azran Hossain <mr69751@gmail.com>
2023-10-05 12:45:16 +0600

    join works

--- portaldv.bergerbd.com/Style%2520Library/JS/Marketing.Dashboard.js
+++ portaldv.bergerbd.com/Style%2520Library/JS/Marketing.Dashboard.js
@@ -1,4 +1,6 @@
-var filteredObjects = [];
+var PendingApprovalData = [];
+var MarketingMasterData = [];
+var TableData = [];
 
 const gridOptions = {
     columnDefs: [
@@ -7,7 +9,7 @@ const gridOptions = {
         { headerName: "Created", field: 'Created', enableRowGroup: true },
         { headerName: "Emp.Id", field: 'AuthorId', maxWidth: 100 },
         { headerName: "Request for", field: 'PendinWith', enableRowGroup: true, minWidth: 300 },
-        { headerName: "Amount", field: 'Amount', maxWidth: 150 },
+        { headerName: "Total Expected Expense", field: 'TotalExpectedExpense', maxWidth: 150 },
         { headerName: "Status", field: 'Status', enableRowGroup: true, minWidth: 300 },
         { headerName: "PendingWith", field: 'PendingWith', enableRowGroup: true },
         { headerName: "View/Action", field: 'View_Action', cellRenderer: viewActionCellRenderer, maxWidth: 150 },
@@ -21,7 +23,7 @@ const gridOptions = {
     flex: 1,
     minWidth: 100,
 
-    paginationPageSize: 5,
+    paginationPageSize: 100,
     suppressRowClickSelection: true,
     groupSelectsChildren: true,
     rowSelection: 'multiple',
@@ -46,54 +48,20 @@ function viewActionCellRenderer(params) {
     return LinkRanderer(params, 'View');
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', async function () {
     var gridDiv = document.querySelector('#dataGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 
-    const base = "https://portaldv.bergerbd.com/leaveauto/_api/web/lists/getByTitle('PendingApproval')/items";
-    const queryx = `$expand=PendingWith,Author&$select=ID,Title,Author/Title,Created,Status,PendingWith/Id,PendingWith/Title`;
+    await FetchPendingApproval();
+    await FetchMarketingMaster();
+    JoinPendingApprovalWithMaster();
 
-    const url = `${base}?&${queryx}`;
-
-    fetch(url, {
-        method: "GET",
-        headers: {
-            "Accept": "application/json;odata=verbose"
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Map the array of objects to create an array of filtered objects
-            filteredObjects = data.d.results.map(info => ({
-                "Id": info.Id,
-                "Title": info.Title,
-                "Status": info.Status,
-                "ID": info.ID,
-                "Created": info.Created,
-                //"PendingWith": info?.PendingWith?.results[0]?.Title || "",
-                "Author": info.Author.Title
-            }));
-            console.log(filteredObjects);
-            gridOptions.api.setRowData(
-                filteredObjects.map((item) => {
-                    return {
-                        ...item,
-                        Created: new Date(item.Created),
-                    }
-                })
-            );
-        })
-        .catch(error => {
-            console.error("Error getting user information", error);
-        })
-        .finally(() => {
-
-        });
+    gridOptions.api.setRowData(TableData.map((item) => {
+        return {
+            ...item,
+            Created: new Date(item.Created),
+        };
+    }));
 });
 
 function LinkRanderer(params, label) {
@@ -111,4 +79,110 @@ function LinkRanderer(params, label) {
     return linkElement;
 }
 
-console.log('hi from override');
+const FetchPendingApproval = async () => {
+    const base = "https://portaldv.bergerbd.com/leaveauto/_api/web/lists/getByTitle('PendingApproval')/items";
+    const queryx = `$expand=PendingWith,Author&$select=ID,Title,Author/Title,Created,Status,PendingWith/Id,PendingWith/Title&$top=20000`;
+
+    const url = `${base}?&${queryx}`;
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json;odata=verbose"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        PendingApprovalData = data.d.results.map(info => ({
+            "Id": info.Id,
+            "Title": info.Title,
+            "Status": info.Status,
+            "ID": info.ID,
+            "Created": info.Created,
+            "Author": info.Author.Title
+        }));
+
+        /* gridOptions.api.setRowData(
+            filteredObjects.map((item) => {
+                return {
+                    ...item,
+                    Created: new Date(item.Created),
+                };
+            })
+        ); */
+    } catch (error) {
+        console.error("Error getting user information", error);
+    }
+};
+
+const FetchMarketingMaster = async () => {
+    const base = "https://portaldv.bergerbd.com/leaveauto/_api/web/lists/getByTitle('MarketingActivityMaster')/items";
+    const queryx = `$select=ID,TotalExpectedExpense&$top=20000`;
+
+    const url = `${base}?&${queryx}`;
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json;odata=verbose"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+
+        MarketingMasterData = data.d.results.map(info => ({
+            "Id": info.Id,
+            "TotalExpectedExpense": info.TotalExpectedExpense
+        }));
+
+        console.log(MarketingMasterData);
+
+    } catch (error) {
+        console.error("Error getting user information", error);
+    }
+
+};
+
+
+const JoinPendingApprovalWithMaster = () => {
+    // Create a new array by matching Id from MarketingMasterData with Title from PendingApprovalData
+    TableData = PendingApprovalData.map(item => {
+        // Extract the integer part from the Title, assuming it's always in the format "IAT-<integer>"
+        const titleInteger = parseInt(item.Title.split('-')[1]);
+
+        // Find the corresponding MarketingMasterData object with the same Id
+        const marketingData = MarketingMasterData.find(data => data.Id === titleInteger);
+
+        // If a matching object is found, add TotalExpectedExpense to the new object
+        if (marketingData) {
+            return {
+                "Author": item.Author,
+                "AuthorId": item.AuthorId,
+                "Title": item.Title,
+                "Status": item.Status,
+                "Created": item.Created,
+                "TotalExpectedExpense": marketingData.TotalExpectedExpense
+            };
+        } else {
+            // If no matching object is found, return a default object
+            return {
+                "Author": item.Author,
+                "AuthorId": item.AuthorId,
+                "Title": item.Title,
+                "Status": item.Status,
+                "Created": item.Created,
+                "TotalExpectedExpense": 0 // or any default value you prefer
+            };
+        }
+    });
+};
